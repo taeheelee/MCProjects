f6e4f29b55f48c2322923e87fa7544dfb7275159
student <student@M110225>
2017-01-17 18:11:34 +0900

    czfdsfdsfd

---
+++ practice/src/practice/RecipeFinder.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class RecipeFinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Start main = new Start();
+		main.Main();		
+	}
+
+}
---
+++ practice/src/practice/UserInfo.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class UserInfo {
+	private String id;
+	private String password;
+	
+	public UserInfo() {
+		id = new String();
+		password = new String();	
+	}
+	public String getId() {	return id;	}
+	public void setId(String id) {	this.id = id;	}
+	public String getPassword() {	return password;	}
+	public void setPassword(String password) {	this.password = password;	}
+}

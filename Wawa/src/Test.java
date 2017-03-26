a51367bc5bc932ff36e9036ce8cb1c1ea524e337
JeonHyeonJun <ddoddoia@naver.com>
2017-03-26 17:18:40 +0900

    Update Test.java

--- Wawa/src/Test.java
+++ Wawa/src/Test.java
@@ -1,60 +1,45 @@
 
 
 import dao.BoardDao;
+import dao.ManagementDao;
 import model.Board;
+import model.Management;
 
 public class Test {
 	public static void main(String[] args) {
-		BoardDao bd = BoardDao.getInstance();
-		Board board = new Board();
+		ManagementDao md = ManagementDao.getInstance();
+		Management management = new Management();
 		
-		board.setBoard_code(1);
-		board.setTitle("제목");
-		board.setWriter("글쓴이");
-<<<<<<< HEAD
-		board.setContent("내용");
-		//board.setReadcount(0); 0으로 자동설정이므로 필요없음
-		//board.setWritedate("now()");	현재시간으로 자동설정이므로 필요없음
-		board.setCategory("카테고리");
-		board.setPetname("펫이름");
-		board.setResist("예방접종");
-		board.setLostdate("20170102");
-		board.setKind("품종");
-		board.setWeight(30);
-		board.setAge(5);
-		board.setSex("수컷");
-		board.setPhone("010-1234-5678");
-		board.setEmail("wawa@naver.com");
-		board.setBirthday("20160412");
-		board.setStarpoint(0);
-		board.setBoastnum(0);
+		management.setCode(123);
+		management.setDate("20160316");
+		management.setWeight(30);
+		md.insertManagement(management);
 		
-		//bd.insertBoard(board);
-		//bd.updateBoard(board);
-		//bd.deleteBoard(1);
-		//System.out.println(bd.selectOne(1));
+//		for(Management m : md.selectList(123))
+//			System.out.println(m);
 		
-//		for(Board b : bd.selectBoardList(1))
-//			System.out.println(b);
+//		BoardDao bd = BoardDao.getInstance();
 		
-		for(Board b : bd.selectAll())
-=======
-		board.setContent("내용임");
-		//board.setReadcount(0); 0으로 자동설정이므로 필요없음
-		//board.setWritedate("now()");	현재시간으로 자동설정이므로 필요없음
-		board.setCategory("카테고리");
-		board.setPetname("펫이름");
-		board.setResist("예방접종");
-		board.setLostdate("20170102");
-		board.setKind("품종");
-		board.setWeight(30);
-		board.setAge(5);
-		board.setSex("수컷");
-		board.setPhone("010-1234-5678");
-		board.setEmail("wawa@naver.com");
-		board.setBirthday("20160412");
-		board.setStarpoint(0);
-		board.setBoastnum(4);
+//		Board board = new Board();
+//		board.setBoard_code(1);
+//		board.setTitle("제목");
+//		board.setWriter("글쓴이");
+//		board.setContent("내용임");
+//		//board.setReadcount(0); 0으로 자동설정이므로 필요없음
+//		//board.setWritedate("now()");	현재시간으로 자동설정이므로 필요없음
+//		board.setCategory("카테고리");
+//		board.setPetname("펫이름");
+//		board.setResist("예방접종");
+//		board.setLostdate("20170102");
+//		board.setKind("품종");
+//		board.setWeight(30);
+//		board.setAge(5);
+//		board.setSex("수컷");
+//		board.setPhone("010-1234-5678");
+//		board.setEmail("wawa@naver.com");
+//		board.setBirthday("20160412");
+//		board.setStarpoint(0);
+//		board.setBoastnum(4);
 		
 //		bd.insertBoard(board);
 
@@ -71,8 +56,7 @@ public class Test {
 //		for(Board b : bd.select_by_keyword("용"))
 //			System.out.println(b);
 		
-		for(Board b : bd.select_by_boastnum())
->>>>>>> branch 'master' of https://github.com/taeheelee/MCProjects.git
-			System.out.println(b);
+//		for(Board b : bd.select_by_boastnum())
+//			System.out.println(b);
 	}
 }

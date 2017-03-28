644ec0d0cd7ffba7fa7d8a44c00dde3c412e17d0
milo37 <milo37@mail.ulsan.ac.kr>
2017-03-28 17:27:48 +0900

    constant

---
+++ Wawa/src/commons/Constant.java
@@ -0,0 +1,44 @@
+package commons;
+
+public class Constant {
+	public static String IDX = "idx";
+	public static String ID = "id";
+	public static String PASSWORD = "password";
+	public static String NICKNAME = "nickName";
+	public static String SEX = "sex";
+	public static String PHONE = "phone";
+	public static String ADMINCHECK = "adminCheck";
+	public static String EMAIL = "email";
+	public static String RESIST = "resist";
+	public static String KIND = "kind";
+	public static String BIRTHDAY = "birthday";
+	public static String NEUTRAL = "neutral";
+	public static String WEIGHT = "weight";
+	public static String GROOMINGSTART = "groomingStart";
+	public static String GROOMINGPERIOD = "groomingPeriod";
+	public static String DATE = "date";
+	public static String BOARDCODE = "boardCode";
+	public static String BOARDIDX = "boardIdx";
+	public static String CONTETNT = "content";
+	public static String TITLE = "title";
+	public static String WRITER = "writer";
+	public static String READCOUNT = "readCount";
+	public static String WRITEDATE = "writeDate";
+	public static String CATEGORY = "category";
+	public static String LOSTDATE = "lostDate";
+	public static String AGE = "age";
+	public static String STARPOINT = "starPoint";
+	public static String LIKE = "like";
+	public static String DOGSIZE = "dogsize";
+	public static String ADULTWEIGHT = "adultWeight";
+	public static String VACCINECODE = "vaccineCode";
+	public static String REALSHOTDATE = "realShotDate";
+	public static String VACCINENAME = "vaccineName";
+	public static String VACCINEPERIOD = "vaccinePeriod";
+	public static String VACCINEAGE = "vaccineAge";
+	public static String IMGIDX = "imgIdx";
+	public static String PHYSICALNAME = "physicalName";
+	public static String PATH = "path";
+	
+	
+}

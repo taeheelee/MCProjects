e4e287a2bc910473729a704a7b22ed894888b214
Mulcam1102 <jhj-jkj@hanmail.net>
2017-03-30 17:47:11 +0900

    x

--- HelloWorld/src/Test.java
+++
@@ -1,8 +0,0 @@
-
-public class Test {
-	public static void main(String[] args) {
-		char s = 'a';
-		String tmp = "ddd";	
-		System.out.println(tmp +s + "a");
-	}
-}
--- practice/src/practice/Food.java
+++
@@ -1,5 +0,0 @@
-package practice;
-
-public class Food {
-	String name;
-}

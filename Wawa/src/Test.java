83a5ddb5c253820d157cc7dc2ac336a1d18f7638
Mulcam1102 <jhj-jkj@hanmail.net>
2017-03-30 17:46:52 +0900

    테스트

--- Wawa/src/Test.java
+++ Wawa/src/Test.java
@@ -3,6 +3,7 @@
 
 import java.util.HashMap;
 
+import commons.Constant;
 import dao.BoardDao;
 import dao.Dog_kindDao;
 import dao.ImageDao;
@@ -18,43 +19,41 @@ public class Test {
 		IBoardDao bd = BoardDao.getInstance();
 		HashMap<String, Object> params = new HashMap<>();
 
-//		params.put("board_code", 2);
-//		params.put("title", "제목이다요");
-//		params.put("writer", "이");
-//		params.put("conten", "게시판노가다다요!");
-//		params.put("category", "카테고리는 개입니다");
-//		params.put("petname", "길승이");
-//		params.put("resist", "레지스트");
-//		params.put("lostdate", "20160317");
-//		params.put("kind", "켈베로스");
-//		params.put("weight", 99);
-//		params.put("age", 1500);
-//		params.put("sex", "중성임!아무튼 중성임!");
-//		params.put("phone", "010-0000-0000");
-//		params.put("email", "Goso@naver.com");
-//		params.put("birthday", "19930114");
-//		params.put("starpoint", 3);
-//		params.put("boastnum", 99);
+//		params.put(Constant.Board.BOARDCODE, 2);
+//		params.put(Constant.Board.TITLE, "제목이다요");
+//		params.put(Constant.Board.WRITER, "이");
+//		params.put(Constant.Board.CONTETNT, "게시판노가다다요!");
+//		params.put(Constant.Board.CATEGORY, "카테고리는 개입니다");
+//		params.put(Constant.Board.NAME, "길승이");
+//		params.put(Constant.Board.RESIST, "레지스트");
+//		params.put(Constant.Board.LOSTDATE, "20160317");
+//		params.put(Constant.Board.KIND, "켈베로스");
+//		params.put(Constant.Board.WEIGHT, 99);
+//		params.put(Constant.Board.AGE, 1500);
+//		params.put(Constant.Board.SEX, "중성임!아무튼 중성임!");
+//		params.put(Constant.Board.PHONE, "010-0000-0000");
+//		params.put(Constant.Board.EMAIL, "Goso@naver.com");
+//		params.put(Constant.Board.BIRTHDAY, "19930114");
+//		params.put(Constant.Board.STARPOINT, 3);
+//		params.put(Constant.Board.LIKECOUNT, 99);
 //		bd.insertBoard(params);
 //		for(HashMap<String, Object> b : bd.selectAllBoard())
 //			System.out.println(b);
 		
 //		IDog_kindDao dd = Dog_kindDao.getInstance();
-//		params.put("kind", "불독2");
-//		params.put("dogSize", "소");
-//		params.put("adultWeight",10);
+//		params.put(Constant.Dog_kind.KIND, "불독2");
+//		params.put(Constant.Dog_kind.DOGSIZE, "소");
+//		params.put(Constant.Dog_kind.ADULTWEIGHT,10);
 //		dd.insertDog_kind(params);
 //		System.out.println(dd.selectOneDog_kind("불독2"));
 //		for(HashMap<String, Object> d : dd.selectAllDog_kind())
 //			System.out.println(d);
 		
 //		IImageDao id = ImageDao.getInstance();
-//		params.put("board_idx", 5);
-//		params.put("physical_name", "기하학");
-//		params.put("name", "화난개구리.jpg");
-//		params.put("path", "니문서");
+//		params.put(Constant.image.PHYSICALNAME, "기하학");
+//		params.put(Constant.image.NAME, "화난개구리.jpg");
+//		params.put(Constant.image.PATH, "니문서");
 //		id.insertImage(params);
-//		System.out.println(id.selectOneImage(2));
 //		for(HashMap<String, Object> i : id.selectAllImage())
 //			System.out.println(i);
 	}

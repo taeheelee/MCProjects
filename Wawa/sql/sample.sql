b30197296e10536daeee8b462a458cd2d40306e1
uheat <ellipsis38@gmail.com>
2017-03-31 15:51:14 +0900

    PetInfo 샘플  5개 정도 만들어 봤습니다.

---
+++ Wawa/sql/sample.sql
@@ -0,0 +1,16 @@
+#petInfo에 대한 샘플
+insert into petinfo values(0,'ABCDE1234567890','test','하랑이','웰시코기','2016-12-25','중성화',2,'male','2017-11-20',60);
+insert into petinfo values(0,'BCDEF1234567890','test','푸돌이','푸들','2016-02-23','해당없음',2,'female','2017-02-23',30);
+insert into petinfo values(0,'CDEFG1234567890','test','황곰이','말티즈','2015-08-20','중성화',2,'male','2017-08-20',90);
+insert into petinfo values(0,'DEFGH1234567890','test','미미','닥스훈트','2016-12-01','해당없음',2,'female','2016-12-20',30);
+insert into petinfo values(0,'DEFGH1234567890','test','와와','치와와','2015-09-13','해당없음',2,'male','2017-08-20',60);
+
+
+#체중관리에 대한 샘플
+insert into management values(0, 1, '2017-01-18', 2.1);
+insert into management values(0, 1, '2017-03-27', 1.6);
+insert into management values(0, 3, '2017-03-27', 1.2);
+insert into management values(0, 4, '2017-03-27', 1.3);
+insert into management values(0, 5, '2017-03-27', 2.0);
+
+

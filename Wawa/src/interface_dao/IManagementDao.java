3b0e02df0ad388923129a35c5e7ad4fce2ff7939
Mulcam1102 <jhj-jkj@hanmail.net>
2017-03-31 16:09:46 +0900

    management 오타수정

--- Wawa/src/interface_dao/IManagementDao.java
+++ Wawa/src/interface_dao/IManagementDao.java
@@ -1,11 +1,12 @@
 package interface_dao;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface IManagementDao {
-	public boolean insertManagement(IManagementDao management);
-	public boolean updateManagement(IManagementDao management);
-	public boolean deleteManagement(int management_idx);
-	public IManagementDao selectOne(int management_idx);
-	public List<IManagementDao> selectList(int code);
+	public int insertManagement(HashMap<String, Object> params);
+	public int updateManagement(HashMap<String, Object> params);
+	public int deleteManagement(int managementIdx);
+	public HashMap<String, Object> selectOne(int managementIdx);
+	public List<HashMap<String, Object>> selectList(int idx);
 }

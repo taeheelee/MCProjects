6e1e3ac73cc62482d85cf3a629e03fbd6ee626e4
milo37 <milo37@mail.ulsan.ac.kr>
2017-04-03 16:38:51 +0900

    selectPet -> selectOne

--- Wawa/src/interface_dao/IPetInfoDao.java
+++ Wawa/src/interface_dao/IPetInfoDao.java
@@ -8,7 +8,7 @@ public interface IPetInfoDao {
 	public int insertPetInfo(HashMap<String, Object> params);
 	public int updatePetInfo(HashMap<String, Object> params);
 	public int deletePetInfo(int idx);
-	public HashMap<String, Object> selectPet(int idx);
+	public HashMap<String, Object> selectOne(int idx);
 	public List<HashMap<String, Object>> selectAll();
 	public HashMap<String, Object> selectVaccine();
 

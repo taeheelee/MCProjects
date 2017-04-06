088ad3ecb3ae2a6d67eb799f2c4d946f487eb4dd
milo37 <milo37@mail.ulsan.ac.kr>
2017-04-06 14:46:34 +0900

    management

--- Wawa/src/interface_service/IManagementService.java
+++ Wawa/src/interface_service/IManagementService.java
@@ -1,5 +1,10 @@
 package interface_service;
 
+import java.util.HashMap;
+
 public interface IManagementService {
+	public boolean insertManagement(HashMap<String, Object> params);
+	public boolean updateManagement(HashMap<String, Object> params);
+	public int deleteManagement(int managementIdx);
 
 }

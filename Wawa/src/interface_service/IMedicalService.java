31af4a946e03f91b36fa48f13d00743ea0b7a26c
uheat <ellipsis38@gmail.com>
2017-04-06 14:19:35 +0900

    예방접종 MedicalService 추가

---
+++ Wawa/src/interface_service/IMedicalService.java
@@ -0,0 +1,18 @@
+package interface_service;
+
+import java.util.Date;
+import java.util.HashMap;
+
+public interface IMedicalService {
+	public boolean insertRealShotDate(HashMap<String, Object> params);
+	public boolean updateRealShotDate(HashMap<String, Object> params);
+	public boolean deleteRealShotDate(HashMap<String, Object> params);
+	
+	//실제접종일 보기
+	public HashMap<String, Object> selectRealShotDate(HashMap<String, Object> params);
+	
+	//추천접종일 보기
+	public HashMap<String, Object> selectDueShotDate(HashMap<String, Object> params);
+	
+
+}

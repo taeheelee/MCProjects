9677f156c5b4914f9ffb8926354040b2d70eb863
milo37 <milo37@mail.ulsan.ac.kr>
2017-04-19 15:14:57 +0900

    main수정

--- Wawa/src/controller/MainController.java
+++ Wawa/src/controller/MainController.java
@@ -1,7 +1,8 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class MainController {
 	@RequestMapping("main.do")
 	public void main(){}

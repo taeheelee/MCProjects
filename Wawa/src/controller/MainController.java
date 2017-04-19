381bbe695be1f86a10e52169fb80aa60e1f512eb
student <student@M90117>
2017-04-19 15:49:23 +0900

    ,.m.,

--- Wawa/src/controller/MainController.java
+++ Wawa/src/controller/MainController.java
@@ -1,5 +1,30 @@
 package controller;
 
-public class MainController {
+import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import interface_service.IJoinService;
+
+@Controller
+public class MainController {
+	@Autowired
+	private IJoinService joinService;
+	
+	@RequestMapping("main.do")
+	public void main(){}
+	
+	@RequestMapping("joinForm.do")
+	public void joinForm(){}
+	
+	@RequestMapping("join.do")
+	public String join(String id, String password, String nickname,
+			String sex, String phoneNum, int adminCheck, String email){
+		joinService.join(id, password, nickname, sex, phoneNum, adminCheck, email);
+		return "redirect:main.do";
+	}
+	
 }

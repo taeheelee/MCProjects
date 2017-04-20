93ece35f625d8d78f09560f3960a7b0181c8a6a0
milo37 <milo37@mail.ulsan.ac.kr>
2017-04-20 11:02:30 +0900

    회원가입 로그인 구현

--- Wawa/src/controller/MainController.java
+++ Wawa/src/controller/MainController.java
@@ -1,17 +1,36 @@
 package controller;
 import java.util.HashMap;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import interface_service.IJoinService;
+import interface_service.IMemberService;
+import model.UserInfo;
 
 @Controller
 public class MainController {
 	@Autowired
-	private IJoinService joinService;
+	private IMemberService iMemberService;
+	
+	@RequestMapping("loginForm.do")
+	public String loginForm(){
+		return "login";
+	}
+	@RequestMapping(method=RequestMethod.POST, value="login.do")
+	public String login(HttpSession session, String id, String pw){
+		UserInfo userInfo = iMemberService.getMember(id);
+		if(iMemberService.loginMember(id, pw)){
+			
+			session.setAttribute("id", userInfo.getId());
+			session.setAttribute("name", userInfo.getNickname());
+		}
+		return "redirect:main.do";
+	}
 	
 	@RequestMapping("main.do")
 	public void main(){}
@@ -22,7 +41,7 @@ public class MainController {
 	@RequestMapping("join.do")
 	public String join(String id, String password, String nickname,
 			String sex, String phoneNum, int adminCheck, String email){
-		joinService.join(id, password, nickname, sex, phoneNum, adminCheck, email);
+		iMemberService.join(id, password, nickname, sex, phoneNum, adminCheck, email);
 		return "redirect:main.do";
 	}
 }
--- Wawa/src/interface_dao/IUserInfoDao.java
+++ Wawa/src/interface_dao/IUserInfoDao.java
@@ -3,11 +3,13 @@ package interface_dao;
 import java.util.HashMap;
 import java.util.List;
 
+import model.UserInfo;
+
 public interface IUserInfoDao {
 	public int insertUserInfo(HashMap<String, Object> params);
 	public int updateUserInfo(HashMap<String, Object> params);
 	public int deleteUserInfo(String id);
-	public HashMap<String, Object> selectOneUserInfo(String id);
+	public UserInfo selectOneUserInfo(String id);
 	public List<HashMap<String, Object>> selectAllUserInfo();
 	public List<HashMap<String, Object>> selectBoardList();
 }

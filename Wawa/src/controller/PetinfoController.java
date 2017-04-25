6840ec6dad53ef22fb7781ae9c6855bd5aebf66e
student <student@M90117>
2017-04-25 12:58:59 +0900

    # WARNING: head commit changed in the meantime
    여 ㅋ 러 ㅋ 가 ㅋ 지

--- Wawa/src/controller/PetinfoController.java
+++ Wawa/src/controller/PetinfoController.java
@@ -1,39 +1,73 @@
 package controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.ModelAndView;
 
-import commons.Constant;
 import interface_service.IPetinfoService;
 
 @Controller
-@SessionAttributes("id")
 public class PetinfoController {
 
 	@Autowired
 	private IPetinfoService petinfoService;
 	
 	@RequestMapping("addPetForm.do")
-	public String addPetForm(){
+	public String addPetForm(HttpSession session){
+	//	session.setAttribute("id", session.);
 		return "addPetForm.tiles";
 	}
 	
 	@RequestMapping("addPet.do")
 	public String addPet(String resist, String id, String name, String kind, 
-			Date birthday, String neutral, double weight, String sex, 
-			Date groomingStart, int groomingPeriod){
-		boolean result = petinfoService.insertPetInfo(0, resist, id, name, kind, birthday, neutral, weight, 
-				sex, groomingStart, groomingPeriod);
+			String birthday, String neutral, double weight, String sex, 
+			String groomingStart, String groomingPeriod){
+		
+		String fromBirth = birthday;
+		SimpleDateFormat transBirthFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date tobirth = null;
+		try {
+			tobirth = transBirthFormat.parse(fromBirth);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String fromGs = groomingStart;
+		SimpleDateFormat transGsFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date toGs = null;
+		try {
+			toGs = transGsFormat.parse(fromGs);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		boolean result = petinfoService.insertPetInfo(0, resist, id,
+				name, kind, tobirth, neutral, weight, 
+				sex, toGs, groomingPeriod);
 		if(result){
 			return "redirect:main.do";			
 		}else {
 			return "redirect:addPetForm.do";
 		}
 	}
+	
+	@RequestMapping("healthcare.do")
+	public String healthcare(){
+		return "healthcare.tiles";
+	}
+	
+	@RequestMapping("medicalcare.do")
+	public String medicalcare(){
+		return "medicalcare.tiles";
+	}
+	
 	
 }

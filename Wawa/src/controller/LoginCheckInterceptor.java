3b9722ea8292a16a06419ccf8ebda4be4cf0a934
uheat <xiaoyoul@naver.com>
2017-05-12 14:02:06 +0900

    login Interceptor 추가 -> 로그인안되있을시 로그인창으로 이동

---
+++ Wawa/src/controller/LoginCheckInterceptor.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class LoginCheckInterceptor extends HandlerInterceptorAdapter{
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+//		return super.preHandle(request, response, handler);
+		
+		if(request.getSession().getAttribute("id") == null){
+			HttpSession session = null;
+			session.setAttribute("notLogin", "not");
+			response.sendRedirect("loginForm.do");
+			
+			return false;
+		}else
+			return true;
+	}
+	
+}
